0fa900900e28329161d637842a19e12b72e3e350
Seth Fitzsimmons <seth@mojodna.net>
2009-06-04 18:12:57 -0700

    capture location input

--- src/net/mojodna/fireeagle/explicit/ExplicitUpdater.java
+++ src/net/mojodna/fireeagle/explicit/ExplicitUpdater.java
@@ -4,9 +4,17 @@ import android.content.Intent;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ExplicitUpdater extends OAuthActivity {
+public class ExplicitUpdater extends OAuthActivity implements OnClickListener,
+		OnKeyListener {
 	/**
 	 * Clear preferences.
 	 */
@@ -26,6 +34,12 @@ public class ExplicitUpdater extends OAuthActivity {
 
 		if (isAuthorized()) {
 			setContentView(R.layout.main);
+
+			Button update = (Button) findViewById(R.id.btnUpdateLocation);
+			update.setOnClickListener(this);
+
+			EditText location = (EditText) findViewById(R.id.txtLocation);
+			location.setOnKeyListener(this);
 		} else {
 			startAuthorization();
 		}
@@ -46,5 +60,37 @@ public class ExplicitUpdater extends OAuthActivity {
 	 */
 	protected void startAuthorization() {
 		startActivity(new Intent().setClass(this, AuthorizationActivity.class));
+	}
+
+	/**
+	 * Submit a location to Fire Eagle
+	 * 
+	 * @param location
+	 *            Location to submit.
+	 */
+	protected void submitLocation(String location) {
+		Log.i("updater", "Location is: " + location);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Log.i("updater", v + " was clicked.");
+		EditText location = (EditText) findViewById(R.id.txtLocation);
+		submitLocation(location.getText().toString());
+	}
+
+	@Override
+	public boolean onKey(View v, int keyCode, KeyEvent event) {
+		// capture newlines and prevent them from displaying
+		if (keyCode == KeyEvent.KEYCODE_ENTER) {
+
+			// submit the location on key up (to avoid duplicate submissions)
+			if (event.getAction() == KeyEvent.ACTION_UP) {
+				submitLocation(((TextView) v).getText().toString());
+			}
+			return true;
+		}
+
+		return false;
 	}
 }
